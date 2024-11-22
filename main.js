cdbcc47df58d0d626c8a6f75d2526a151d189d30
Juan David Muñoz Rojas <juanmunozrojas95@gmail.com>
2024-11-22 14:13:34 -0500

    primer commit del proyecto

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 1200,
+        height: 1000,
+        minWidth: 700,
+        minHeight: 500
+    })
+
+    window.loadFile('src/view/index.html')
+}
+
+app.whenReady().then (() =>{
+    createWindow()
+})
+
